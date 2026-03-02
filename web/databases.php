ba42ac73234ab8d875896ffbd886c99fbb213772
libdevel <libdevel@gmail.com>
2026-03-02 16:26:56 -0500

    accessibility fixes

--- web/databases.php
+++ web/databases.php
@@ -33,119 +33,92 @@
                   <!-- highlighted -->
                   <div class="region-content">
 <h1 class="page-header">Databases by Subject</h1>
-
-<div class="card mb-3">
-<div class="card-header">
-    <h2><a href="https://sunybroome.libguides.com/az/databases?s=169898">Great Places to Start</a></h2>
-</div><!-- card header-->
-<div class="card-body">
+<p>
+    Search the library databases for articles in scholarly journals, magazines and newspapers.
+</p>
+<h2>
+    Subjects
+</h2>
 <ul>
-    <li><a href="https://sunybroome.libguides.com/az/databases?s=253973">Get Started</a></li>
-    <li><a href="https://sunybroome.libguides.com/az/databases?s=169896">Current Events: Pros and Cons</a></li>
-    <li><a href="https://sunybroome.libguides.com/az/databases?s=169894">News</a></li>
-    <li><a href="https://sunybroome.libguides.com/az/databases?s=166985">Primary Sources</a></li>
-
+    <li data-list-item-id="ee70e79ad895fc733605cb8ec54e03e40">
+        <a href="https://sunybroome.libguides.com/az/databases?s=225043">Art and Design</a>
+    </li>
+    <li data-list-item-id="e7926b9a70389b6c8d6affa23c779cd5a">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169880">Business and Professional Studies</a>
+    </li>
+    <li data-list-item-id="e872cb8fb913ebefdaf0a2e9934dd1643">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169900">Career and Job Resources</a>
+    </li>
+    <li data-list-item-id="e9b4126134f62f8bb7de0197458493c79">
+        <a href="https://sunybroome.libguides.com/az/databases?s=166949">Citations</a>
+    </li>
+    <li data-list-item-id="e8b7067bb90d1acba51dbcd5cadf331f9">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169889">Communications and Media Arts</a>
+    </li>
+    <li data-list-item-id="e59a0aab686cd8d7cab8539542c7bfbd1">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169881">Criminal Justice and Emergency Services</a>
+    </li>
+    <li data-list-item-id="ed8a2bf3fa664a049dcd26341316cba47">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169882">Data and Statistics</a>
+    </li>
+    <li data-list-item-id="eec3dbcec6451c943375b77f2a795766f">
+        <a href="https://sunybroome.libguides.com/az/databases?s=209799">Diversity</a>
+    </li>
+    <li data-list-item-id="e8650e97c3fb52c32fa2fe8be3c959d74">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169899">eBooks</a>
+    </li>
+    <li data-list-item-id="e4d220fedb6f8b3edff2ed3e80cc3c13b">
+        <a href="https://sunybroome.libguides.com/az/databases?s=205604">English and Literature</a>
+    </li>
+    <li data-list-item-id="ef49959bf53e6009eef03d1e6becc3f00">
+        <a href="https://sunybroome.libguides.com/az/databases?s=183477">Foreign Languages, ESL, and Speech</a>
+    </li>
+    <li data-list-item-id="e38377ffe4ea56d333276c085a948c6a2">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169898">General and Reference</a>
+    </li>
+    <li data-list-item-id="eaef67d30819992abf5fd8c9dfb3affdd">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169885">Health Sciences</a>
+    </li>
+    <li data-list-item-id="e8068915f9888c61f963fb088fc69f8ad">
+        <a href="https://sunybroome.libguides.com/az/databases?s=253560">History</a>
+    </li>
+    <li data-list-item-id="e2476a3fd7eee38af29ca516c204e2813">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169886">History, Philosophy, and Social Sciences</a>
+    </li>
+    <li data-list-item-id="eae5c80586ce7170cee2c7c132d97085a">
+        <a href="https://sunybroome.libguides.com/az/databases?s=225041">Hospitality Programs</a>
+    </li>
+    <li data-list-item-id="e4c148833b656490623e542c60ea0d18c">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169884">Music and Theater Arts</a>
+    </li>
+    <li data-list-item-id="ebf416ad047475fe98ff8073d964c6846">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169894">News</a>
+    </li>
+    <li data-list-item-id="ef82f8967c1203862a36c343b8eb50bbe">
+        <a href="https://sunybroome.libguides.com/az/databases?s=225051">Nursing</a>
+    </li>
+    <li data-list-item-id="ec7804bea5ab37c16909c91d7de93f0c4">
+        <a href="https://sunybroome.libguides.com/az/databases?s=223862">Physical Education &amp; Sport Studies</a>
+    </li>
+    <li data-list-item-id="e61c2e73dcef83a57cc5f3f69c080e8a6">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169895">Politics and Government</a>
+    </li>
+    <li data-list-item-id="ed98fc2d7b0c7df889116bf2382b74ecf">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169890">Psychology and Human Services</a>
+    </li>
+    <li data-list-item-id="ef65bfd257f7daeb31889d0f42588b702">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169891">STEM</a>
+    </li>
+    <li data-list-item-id="e9988da06ce6cb4e2bf237da9c76f85a7">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169896">Social Issues: Pros and Cons</a>
+    </li>
+    <li data-list-item-id="e6e271c0d07c0a1287c4cfe37338c4b41">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169892">Social Science</a>
+    </li>
+    <li data-list-item-id="e6590ea56563d91eec178702201b7f1be">
+        <a href="https://sunybroome.libguides.com/az/databases?s=169883">Teacher Education and Early Childhood Education</a>
+    </li>
 </ul>
-</div><!-- card body-->
-</div><!-- /card -->
-
-<div class="row">
-    <div class="col-md-6 mb-2">
-    <div class="card">
-    <div class="card-header">
-        <h2><a href="https://sunybroome.libguides.com/bps">Business and Professional Studies</a></h2>
-    </div><!-- card header-->
-    <div class="card-body">
-    <ul>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=225044">Business</a></li>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=225040">Business Information Technology</a></li>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=169881">Criminal Justice and Emergency Services</a></li>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=225041">Hospitality Programs</a></li>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=224182">Medical Assisting and Health Studies</a></li>
-    </ul>
-    </div><!-- card body-->
-    </div><!-- /card -->
-        </div><!-- col -->
-
-    <div class="col-md-6 mb-2">
-    <div class="card">
-    <div class="card-header">
-        <h2><a href="https://sunybroome.libguides.com/az/databases?s=169885">Health Sciences</a></h2>
-    </div><!-- card header-->
-    <div class="card-body">
-    <ul>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=225050">Clinical Laboratory Technologies</a></li>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=224181">Dental Hygiene</a></li>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=225051">Nursing</a></li>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=225045">Physical Therapist Assistant</a></li>
-        <li><a href="https://sunybroome.libguides.com/az/databases?s=224183">Radiologic Technology</a></li>
-    </ul>
-    </div><!-- card body-->
-    </div><!-- /card -->
-    </div><!-- col -->
- </div><!-- /row -->
-
-    <div class="row">
-           <div class="col-md-6 mb-2">
-        <div class="card">
-        <div class="card-header">
-            <h2><a href="https://sunybroome.libguides.com/az/databases?s=169887">Liberal Arts</a></h2>
-        </div><!-- card header-->
-        <div class="card-body">
-        <ul>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=225043">Art and Design</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=169889">Communications and Media Arts</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=205604">English and Literature</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=183477">Foreign Languages, ESL, and Speech</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=253560">History</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=169886">History, Philosophy, and Social Sciences</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=169884">Music and Theater Arts</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=223862">Physical Education &amp; Sport Studies</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=169890">Psychology and Human Services</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=169895">Politics and Government</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=169892">Social Science</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=169883">Teacher Education and Early Childhood Education</a></li>
-        </ul>
-        </div><!-- card body-->
-        </div><!-- /card -->
-        </div><!-- col -->
-
-        <div class="col-md-6 mb-2">
-        <div class="card">
-        <div class="card-header">
-            <h2><a href="https://sunybroome.libguides.com/stem">STEM</a></h2>
-        </div><!-- card header-->
-        <div class="card-body">
-        <ul>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=225048">Biology</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=225049">Chemistry</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=225052">Computer Science</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=225046">Engineering Science and Physics</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=205605">Engineering Technologies</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=225053">Mathematics</a></li>
-            <li><a href="https://sunybroome.libguides.com/az/databases?s=184906">Physical Science</a></li>
-        </ul>
-        </div><!-- card body-->
-        </div><!-- /card -->
-        </div><!-- col -->
-
-</div><!-- /row -->
-    <div class="col-md-6 mb-2">
-<div class="card">
-<div class="card-header">
-    <h2><a href="https://sunybroome.libguides.com/az/databases?s=251982">Other</a></h2>
-</div><!-- card header-->
-<div class="card-body">
-<ul>
-    <li><a href="https://sunybroome.libguides.com/az/databases?s=169900">Career and Job Resources</a></li>
-    <li><a href="https://sunybroome.libguides.com/az/databases?s=166949">Citations</a></li>
-    <li><a href="https://sunybroome.libguides.com/az/databases?s=169882">Data and Statistics</a></li>
-    <li><a href="https://sunybroome.libguides.com/az/databases?s=209799">Diversity</a></li>
-    <li><a href="https://sunybroome.libguides.com/az/databases?s=169899">eBooks</a></li>
-</ul>
-</div><!-- card body-->
-</div><!-- /card -->
-    </div><!-- col -->
 
 
                     
